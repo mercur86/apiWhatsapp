93478940cdc8f143004ffc06c1e61110dba12573
mercur86 <67831451+mercur86@users.noreply.github.com>
2023-12-29 23:06:45 -0500

    mejora de ruta

--- src/infrastructure/repositories/ws.external.ts
+++ src/infrastructure/repositories/ws.external.ts
@@ -63,9 +63,11 @@ class WsTransporter extends Client implements LeadExternal {
   }
 
   private generateImage = (base64: string) => {
-    const path = `${process.cwd()}/tmp`;
+    //const path = `${process.cwd()}/tmp`;
+    const basePath = 'tmp'; //para produccion 
+    console.log('ruta temp '+basePath);
     let qr_svg = imageQr(base64, { type: "svg", margin: 4 });
-    qr_svg.pipe(require("fs").createWriteStream(`${path}/qr.svg`));
+    qr_svg.pipe(require("fs").createWriteStream(`${basePath}/qr.svg`));
     console.log(`⚡ Recuerda que el QR se actualiza cada minuto ⚡`);
     console.log(`⚡ Actualiza F5 el navegador para mantener el mejor QR ⚡`);
   };
